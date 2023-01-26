fbe51666bef2fab2e1c7d97476903c864cbe25b0
Harsh <harshgovind18@gmail.com>
2023-01-26 18:02:05 +0530

    All changes done -- V3

--- js/scr_comm.js
+++ js/scr_comm.js
@@ -3,34 +3,34 @@
     let timeline = new TimelineMax();
     timeline
         .from('#img1', 4, {
-            y: -100,
-            x: 0,
+            y: 300,
+            x: -300,
             ease: Power3.easeInOut
         })
 
         .from('#img2', 4, {
-        y: 30,
-        x: 60,
+        y: 150,
+        x: -60,
         ease: Power3.easeInOut
     }, '-=4')
     .from('#img3', 4, {
         y: -30,
-        x: 200,
+        x: -200,
         ease: Power3.easeInOut
     }, '-=4')
     .from('#img4', 4, {
         y: -100,
-        x: 0,
+        x: 40,
         ease: Power3.easeInOut
     }, '-=4')
     .from('#img5', 4, {
-        y: -80,
-        x: -20,
+        y: -135,
+        x: -100,
         ease: Power3.easeInOut
     }, '-=4')
     .from('#img6', 4, {
         y: -100,
-        x: 23,
+        x: -80,
         ease: Power3.easeInOut
     }, '-=4')
     
@@ -38,8 +38,10 @@
     let scene = new ScrollMagic.Scene({
         triggerElement: '#committees-sec',
         triggerHook: 0,
-        duration: '100%'
+        duration: '130%'
     })
     .setTween(timeline)
     .setPin('#committees-sec')
     .addTo(controller);
+
+   
--- js/scroll.js
+++ js/scroll.js
@@ -1,16 +1,16 @@
 const hero = document.getElementById('header');
 const heroBG = document.getElementById('heroBG');
 const heroLogo = document.getElementById('Title');
+const heroTitle = document.getElementById('title_ddd');
 
 window.addEventListener('scroll', function () {
 	hero.style.backgroundSize = 100 + +window.pageYOffset / 8 + '%';
 	heroBG.style.opacity = 0 + +window.pageYOffset / 380 + '';
 	heroLogo.style.opacity = 1 - +window.pageYOffset / 200 + '';
+	heroTitle.style.opacity = 1 - +window.pageYOffset / 200 + '';
 })
 
 window.addEventListener('scroll', reveal);
-window.addEventListener('scroll', btnrevel);
-
 
 function reveal() {
 	var reveals = document.getElementsByClassName('reveal');
