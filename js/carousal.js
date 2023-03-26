2d0bbf3c8c25063a1f1a05139f7af11496f5226c
Harsh <harshgovind18@gmail.com>
2023-03-26 17:04:03 +0530

    btns added to carousal

--- js/carousal.js
+++ js/carousal.js
@@ -1,30 +1,69 @@
 
 const track = document.getElementById("image-track");
+track.dataset.percentage = "0";
+track.dataset.prevPercentage = "0";
 
 const handleOnDown = e => track.dataset.mouseDownAt = e.clientX;
 
 const handleOnUp = () => {
-  track.dataset.mouseDownAt = "0";  
+  track.dataset.mouseDownAt = "0";
   track.dataset.prevPercentage = track.dataset.percentage;
 }
 
 const handleOnMove = e => {
-  if(track.dataset.mouseDownAt === "0") return;
-  
+  if (track.dataset.mouseDownAt === "0") return;
+
   const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
-        maxDelta = window.innerWidth/1.1 ;
-  
+    maxDelta = window.innerWidth / 1.1;
+
   const percentage = (mouseDelta / maxDelta) * -100,
-        nextPercentageUnconstrained = parseFloat(track.dataset.prevPercentage) + percentage,
-        nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100);
-  
+    nextPercentageUnconstrained = parseFloat(track.dataset.prevPercentage) + percentage,
+    nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100);
+
   track.dataset.percentage = nextPercentage;
-  
+
   track.animate({
     transform: `translate(${nextPercentage}%, -50%)`
   }, { duration: 2400, fill: "forwards" });
-  
-  for(const image of track.getElementsByClassName("image")) {
+
+  for (const image of track.getElementsByClassName("image")) {
+    image.animate({
+      objectPosition: `${100 + nextPercentage}% center`
+    }, { duration: 1000, fill: "forwards" });
+  }
+}
+
+function next() {
+  const percentage = 12,
+    nextPercentageUnconstrained = parseFloat(track.dataset.prevPercentage) + percentage,
+    nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100);
+
+  track.dataset.percentage = nextPercentage;
+
+  track.animate({
+    transform: `translate(${nextPercentage}%, -50%)`
+  }, { duration: 1000, fill: "forwards" });
+
+  for (const image of track.getElementsByClassName("image")) {
+    image.animate({
+      objectPosition: `${(100 + nextPercentage)}% center`
+    }, { duration: 1000, fill: "forwards" });
+  }
+}
+
+function prev() {
+
+  const percentage = -12,
+    nextPercentageUnconstrained = parseFloat(track.dataset.prevPercentage) + percentage,
+    nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100);
+
+  track.dataset.percentage = nextPercentage;
+
+  track.animate({
+    transform: `translate(${nextPercentage}%, -50%)`
+  }, { duration: 1000, fill: "forwards" });
+
+  for (const image of track.getElementsByClassName("image")) {
     image.animate({
       objectPosition: `${100 + nextPercentage}% center`
     }, { duration: 1000, fill: "forwards" });
