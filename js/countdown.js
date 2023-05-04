52e598ec4b7f50bda9b0b21f4cddc238049bc5e7
Harsh Govind <harshgovind18@gmail.com>
2023-05-04 17:11:17 +0530

    Minor Fixes

--- js/countdown.js
+++ js/countdown.js
@@ -4,7 +4,7 @@ const min = document.querySelector(".min .numb");
 const sec = document.querySelector(".sec .numb");
 var timer = setInterval(() => {
     var currentDate = new Date().getTime();
-    var launchDate = new Date('July 28, 2023 00:00:00').getTime();
+    var launchDate = new Date('July 27, 2023 00:00:00').getTime();
     var duration = launchDate - currentDate;
     var days = Math.floor(duration / (1000 * 60 * 60 * 24));
     var hours = Math.floor((duration % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
