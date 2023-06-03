08973c23a47b191c2c0b087c837cb1b2f69fa7eb
Akshat Goel <goelakshat653@gmail.com>
2023-06-03 15:19:37 +0530

    Final changes (hope so)

--- js/formval.js
+++ js/formval.js
@@ -1,3 +1,40 @@
-function formvali(){
-    
+function ValidateForm(){
+    var returnVal = true;
+    clearErrors();
+
+    var name = document.forms["form"]["name"].value;
+    if(name.length == 0){
+        seterror("name", "Length of name cannot be zero!!")
+        returnVal = false
+    }
+
+    var phone = document.forms["form"]["phone"].value;
+    var email = document.forms["form"]["email"].value;
+    if(email.includes(".com") == false){
+        seterror("email", "Email is invalid")
+
+    }
+    if(phone.length != 10){
+        seterror("phone", "Phone number should have 10 digits")
+    }
+    console.log(phone)
+}
+
+function seterror(id, error){
+    //sets error inside tag of id 
+    element = document.getElementById(id);
+    console.log(element)
+    element.getElementsByClassName('formerror').innerText = error;
+    alert(error)
+
+}
+function clearErrors(){
+
+    errors = document.getElementsByClassName('formerror');
+    for(let item of errors)
+    {
+        item.innerHTML = "";
+    }
+
+
 }
